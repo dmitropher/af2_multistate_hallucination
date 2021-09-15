21ee1cbe8054a83a55b5110413ec4ac24bad4096
dzorine <d.zorine@gmail.com>
2021-09-15 13:09:43 -0700

    migrated objects, changed imports to expilicit

--- modules/file_io.py
+++ modules/file_io.py
@@ -1,5 +1,5 @@
 import os
-import io
+import json
 import sys
 import tempfile
 
@@ -11,8 +11,20 @@ def default_dssp_dict():
     """
     Return the dssp dict file
     """
-    hdf5_path = os.path.join(os.path.dirname(__file__), "resources/dssp")
-    return hdf5_path
+    dssp_dict_path = os.path.join(os.path.dirname(__file__), "resources/dssp")
+    return dssp_dict_path
+
+
+def default_aa_freq():
+    """
+    Return the default aa frequency dict
+    """
+    aa_freq_json_path = os.path.join(
+        os.path.dirname(__file__), "resources/default_aa_freq.json"
+    )
+    with open(aa_freq_json_path, "r") as f:
+        aa_freqs_dict = json.load(f)
+    return aa_freqs_dict
 
 
 def dummy_pdbfile(oligo):
