9998219b836f76c6accfa0ff84e1a14ea1d49a05
dzorine <dzorine@gmail.com>
2021-09-17 15:57:49 -0700

    rosetta 3.8 path instead of 3.7

--- modules/rosetta_losses.py
+++ modules/rosetta_losses.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 
-sys.path.append("/software/conda/envs/pyrosetta/lib/python3.7/site-packages/")
+sys.path.append("/software/pyrosetta3.8/latest/")
 import pyrosetta
 
 from file_io import dummy_pdbfile
