5f653a031293c47abb8493045d82ccd189f4d6c4
dmitropher <dzorine@gmail.com>
2021-09-17 14:35:17 -0700

    updated distinction between score/value
    The new distinction is much more hardline: value is just for the actual 
    value derived by the function. Score is now the only way to get the loss 
    rescaled between 0-1 (unless the value happens to be this).
    
     Combination losses are updated to reflect this, and use loss.score() 
    instead of loss.value, hopefully caught all of the ones where I needed 
    to invert min/max or change default invert value.
    
    TODO, many of the losses would be better as configurable logistic 
    functions about some mean with a configurable linear range

--- modules/rosetta_losses.py
+++ modules/rosetta_losses.py
@@ -4,6 +4,7 @@ import pyrosetta
 
 from file_io import dummy_pdbfile
 from loss_factory import Loss, get_loss_creator
+from simple_losses import weightedLoss, dualLoss, maxLoss
 
 
 class SAPLoss(Loss):
@@ -55,7 +56,47 @@ class SAPLoss(Loss):
         return 1 - rescaled
 
 
-global_losses_dict = {"sap_loss": SAPLoss}
+class SAPPlusDual(weightedLoss):
+    def __init__(self, oligo_obj=None, **user_kwargs):
+        """
+        init for this combined loss
+
+        The default args for weights in the super init take into account that dual
+        is an equally weighted combined score from two values, hence the 1/3,2/3 instead of "even"
+        """
+        super().__init__(
+            dualLoss(oligo_obj=oligo_obj, loss_name="dual"),
+            SAPLoss(oligo_obj=oligo_obj, loss_name="sap_loss", **user_kwargs),
+            weights={"sap_loss": (1 / 3), "dual": (2 / 3)},
+            even=False,
+            invert=False,
+            **user_kwargs,
+        )
+        self._information_string = f"""Three part equally weighted loss, using SAP, pLDDT, and ptm
+        0-1, lower is better.
+        SAP is weighted according to the SAPLoss object config. The SAPLoss component inherits kwargs from this at time of writing"""
+
+
+class MaxSAPDual(maxLoss):
+    def __init__(self, oligo_obj=None, **user_kwargs):
+        """
+        init for this combined loss
+
+        The default args for weights in the super init take into account that dual
+        is an equally weighted combined score from two values, hence the 1/3,2/3 instead of "even"
+        """
+        super().__init__(
+            dualLoss(oligo_obj=oligo_obj, loss_name="dual"),
+            SAPLoss(oligo_obj=oligo_obj, loss_name="sap_loss", **user_kwargs),
+            invert=False,
+            **user_kwargs,
+        )
+        self._information_string = f"""Three part loss, using SAP, pLDDT, and ptm: takes the worst from the three
+        0-1, lower is better.
+        SAP is weighted according to the SAPLoss object config. The SAPLoss component inherits kwargs from this at time of writing"""
+
+
+global_losses_dict = {"sap_loss": SAPLoss, "sap_plddt_ptm_equal": SAPPlusDual}
 
 
 def get_loss_dict():
