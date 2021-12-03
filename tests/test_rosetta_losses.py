0b7f2b33a5c16b9aeab4ef3798ea6f05a1eb2e7c
dmitropher <dzorine@gmail.com>
2021-12-02 21:35:13 -0800

    adding tests, broken state

--- tests/test_rosetta_losses.py
+++ tests/test_rosetta_losses.py
@@ -1,94 +1,86 @@
-import sys
+import os
 
-import numpy as np
-
-sys.path.append("/software/pyrosetta3.8/latest/")
+from unittest.mock import Mock, MagicMock
+from rosetta_losses import (
+    CyclicSymmLoss,
+    SAPLoss,
+    CyclicPlusDual,
+    SAPPlusDual,
+    MaxSAPDual,
+)
+from transform_decomposition import (
+    helical_axis_data,
+    helical_axis_to_rise_rotation_radius_axis,
+)
+from file_io import dummy_pdbfile
 import pyrosetta
 
-from file_io import dummy_pdbfile
-from loss_factory import Loss, get_loss_creator
-from simple_losses import weightedLoss, dualLoss, maxLoss
-from transform_decomposition import helical_axis_data
+from alphafold.protein import Protein
 
 
-class CyclicSymmLoss(Loss):
-    """
-    Value is the SAP score of the protein oligo when loaded
-    """
+class TestCyclicSymmLoss:
+    def mock_oligo_from_pdb(self, pdb_path):
+        test_protein = Protein()
+        with open(pdb_path, "r") as f:
+            pdb_str = f.read()
+            test_protein.from_pdb_string(pdb_str)
+        test_oligo = Mock()
+        test_oligo.try_unrelaxed_structure = test_protein
+        return test_oligo
 
-    def __init__(self, oligo_obj=None, **params):
-        super().__init__(oligo_obj=None, **params)
-        self.oligo = oligo_obj
+    def setup(self):
+        resources = os.path.dirname(__file__) + "test_resources"
+        self.monkey_pdb_path = resources + "/monkey.pdb"
+        self.good_c3_pdb_path = resources + "/good_c3.pdb"
+        test_oligo = Mock()
+        test_oligo.try_unrelaxed_structure = self.mock_oligo_from_pdb(
+            self.monkey_pdb_path
+        )
+        test_oligo_good_c3 = Mock()
+        test_oligo_good_c3.try_unrelaxed_structure = self.mock_oligo_from_pdb(
+            self.good_c3_pdb_path
+        )
+        self.cyc_loss = CyclicSymmLoss(oligo_obj=test_oligo)
+        self.cyc_loss_good_c3 = CyclicSymmLoss(oligo_obj=test_oligo_good_c3)
+
+    def test_compute(self):
+
         pyrosetta.distributed.maybe_init()
-        self._n_repeats = len(self.oligo.subunits)
-        self.value = self.compute()
-        self._information_string = f"""This loss computes deviation from ideal cyclic.
-        Score rescales it between 0-1, lower is better"""
 
-    def compute(self):
-
-        dummy = dummy_pdbfile(self.oligo)
-        dummy_path = dummy.name
-        pose = pyrosetta.pose_from_file(dummy_path)
+        pose = pyrosetta.pose_from_file(self.test_pdb_path)
 
         s, C, theta, d2, dstar = helical_axis_data(pose, self._n_repeats)
-        self._params_dict = {
-            "axis_direction": s,
-            "axis_point": C,
-            "rotation_about": theta,
-            "d2": d2,
-            "dstar": dstar,
-            "rise": np.dot(d2 / np.linalg.norm(d2), d2 + dstar),
-        }
+        rise, rotation, s, C, radius = helical_axis_to_rise_rotation_radius_axis(
+            s, C, theta, d2, dstar
+        )
 
-        self.value = [
-            self._params_dict["axis_direction"],
-            self._params_dict["axis_point"],
-            self._params_dict["rotation_about"],
-            self._params_dict["d2"],
-            self._params_dict["dstar"],
-            self._params_dict["rise"],
+        assert self.cyc_loss.value == [
+            self.cyc_loss._params_dict["axis_direction"],
+            self.cyc_loss._params_dict["axis_point"],
+            self.cyc_loss._params_dict["rotation_about"],
+            self.cyc_loss._params_dict["d2"],
+            self.cyc_loss._params_dict["dstar"],
+            self.cyc_loss._params_dict["rise"],
         ]
+        assert self.cyc_loss._params_dict["axis_direction"] == s
+        assert self.cyc_loss._params_dict["axis_point"] == C
+        assert self.cyc_loss._params_dict["rotation_about"] == theta
+        assert self.cyc_loss._params_dict["d2"] == d2
+        assert self.cyc_loss._params_dict["dstar"] == dstar
+        assert self.cyc_loss._params_dict["rise"] == rise
 
-        return self.value
+    def test_score(self):
+        assert 0 < self.cyc_loss.score() < 1
+        assert self.cyc_loss_good_c3 < self.cyc_loss
 
-    def score(self):
-        self.compute()
-        d_rotation = np.degrees(
-            abs(
-                self._params_dict["rotation_about"]
-                - (np.pi * 2 / self._n_repeats)
-            )
-        )
-
-        rescaled_theta = self.logistic_rescale(4, 1, 1.5, val=d_rotation)
-
-        rescaled_rise = self.logistic_rescale(
-            2, 1, 2, val=abs(self._params_dict["rise"])
-        )
-
-        return (rescaled_rise + rescaled_theta) / 2
-
-    def get_base_values(self):
-        name_dict = {self.loss_name: self.score()}
-        data_dict = {
-            "d2": np.linalg.norm(self._params_dict["d2"]),
-            "dstar": np.linalg.norm(self._params_dict["dstar"]),
-            "rise": np.linalg.norm(self._params_dict["rise"]),
-            "d_rotation": np.degrees(
-                abs(
-                    self._params_dict["rotation_about"]
-                    - (np.pi * 2 / self._n_repeats)
-                )
-            ),
-            "raw_rotation": np.degrees(self._params_dict["rotation_about"]),
-        }
-
-        all_dict = {**data_dict, **name_dict}
-        return all_dict
+    def test_get_base_values(self):
+        test_keys = self.cyc_loss.get_base_values().keys()
+        required_keys = ["d2", "dstar", "rise", "d_rotation", "raw_rotation"]
+        for key in required_keys:
+            assert key in test_keys
 
 
-class CyclicPlusDual(weightedLoss):
+class TestCyclicPlusDual:
     def __init__(
         self, loss_name="cyclic_dual_2t1", oligo_obj=None, **user_kwargs
     ):
@@ -108,7 +100,7 @@ class CyclicPlusDual(weightedLoss):
         self._information_string = f"""Three part loss, cyclic, plddt, ptm. Cyclic is 2:1 weighted above the average of the other two"""
 
 
-class SAPLoss(Loss):
+class TestSAPLoss:
     """
     Value is the SAP score of the protein oligo when loaded
     """
@@ -157,7 +149,7 @@ class SAPLoss(Loss):
         return rescaled
 
 
-class SAPPlusDual(weightedLoss):
+class TestSAPPlusDual:
     def __init__(
         self, loss_name="sap_plddt_ptm_equal", oligo_obj=None, **user_kwargs
     ):
@@ -180,7 +172,7 @@ class SAPPlusDual(weightedLoss):
         SAP is weighted according to the SAPLoss object config. The SAPLoss component inherits kwargs from this at time of writing"""
 
 
-class MaxSAPDual(maxLoss):
+class TestMaxSAPDual:
     def __init__(
         self, loss_name="sap_plddt_ptm_max", oligo_obj=None, **user_kwargs
     ):
@@ -196,34 +188,3 @@ class MaxSAPDual(maxLoss):
         self._information_string = f"""Three part loss, using SAP, pLDDT, and ptm: takes the worst from the three
         0-1, lower is better.
         SAP is weighted according to the SAPLoss object config. The SAPLoss component inherits kwargs from this at time of writing"""
-
-
-global_losses_dict = {
-    "sap_loss": SAPLoss,
-    "sap_plddt_ptm_equal": SAPPlusDual,
-    "sap_plddt_ptm_max": MaxSAPDual,
-    "cyclic_symm_loss": CyclicSymmLoss,
-    "cyclic_dual_2t1": CyclicPlusDual,
-}
-
-
-def get_loss_dict():
-    return global_losses_dict
-
-
-def get_global_creator():
-    global_creator = get_loss_creator(**get_loss_dict())
-    return global_creator
-
-
-def get_loss(loss_name, **loss_params):
-    return get_global_creator().get_loss(loss_name, **loss_params)
-
-
-def get_allowed_rosetta_loss_names():
-    """
-    Returns the DSSP string values registered with the current creator
-    """
-    from copy import deepcopy
-
-    return deepcopy(get_global_creator()._creators)
