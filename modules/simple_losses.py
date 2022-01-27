627ae6943b2f203f9087d52a73aad7e9ca53bcb4
dzorine <dzorine@gmail.com>
2022-01-26 18:21:42 -0800

    fixed separation loss missing oligo_obj

--- modules/simple_losses.py
+++ modules/simple_losses.py
@@ -252,13 +252,14 @@ class separationLoss(Loss):
 
         super().__init__(oligo_obj=oligo_obj, **user_kwargs)
         self.value = self.compute()
+        self.oligo_obj = oligo_obj
         self._information_string = f"""This loss object for: {self.loss_name}.
         A geometric term that forces a cyclic symmetry. A PDB is generated, and the distance between the center of mass of adjacent units computed.
         The standard deviation of these neighbour distances is the loss.
         Score is separation dist std: 0+ ,lower is better"""
 
     def compute(self):
-        self.value = get_separation_std(self.oligo)
+        self.value = get_separation_std(self.oligo_obj)
         return self.value
 
 
